50af910950332177fa25fe947dbec5345a6ed311
unknown <dc3016@srmist.edu.in>
2022-09-10 03:32:12 +0530

    Navbar added with few minor changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,13 +7,14 @@ import { Avatar } from '@mui/material';
 
 const Navbar = () => {
   return (
+    <>
     <div className='flex justify-between sticky top-0 bg-white  w-[100%]'>
       <img className=" object-contain h-[100px] ml-20" 
       src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"  alt=""/>
 
       <div className=' flex flex-1 max-w-fit  items-center  p-[20px] h-[30px] m-auto  rounded-full boreder border-[#DDDDDD]  border-2 shadow-lg'>
         <input className=' border-none border-white w-64 outline-0' type="text" />
-        <SearchIcon/>
+        <SearchIcon className=' rounded-full p-1 text-white bg-[#FF385C]'/>
       </div>
       <div className='flex items-center justify-between w-[20vw] mr-20'>
         <p className=' text-base font-bold'>Become a host</p>
@@ -24,6 +25,8 @@ const Navbar = () => {
         </div>
       </div>
     </div>
+    <hr className='w-[100%] border-[#DDDDDD] shadow-md '/>
+    </>
   )
 }
 
