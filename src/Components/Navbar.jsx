2c23710e1ad9ba387f4ff864f5c8d8f5bbe70e6d
unknown <dc3016@srmist.edu.in>
2022-09-10 14:47:45 +0530

    new Navbar with  changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,26 +7,32 @@ import { Avatar } from '@mui/material';
 
 const Navbar = () => {
   return (
-    <>
-    <div className='flex justify-between sticky top-0 bg-white  w-[100%]'>
-      <img className=" object-contain h-[100px] ml-20" 
+    <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
+      {/* left */}
+    <div className='relative flex items-center h-10 cursor-pointer my-auto'>
+      <img className=" object-contain object-left h-[100px]"
+      layout="fill" 
       src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"  alt=""/>
-
-      <div className=' flex flex-1 max-w-fit  items-center  p-[20px] h-[30px] m-auto  rounded-full boreder border-[#DDDDDD]  border-2 shadow-lg'>
-        <input className=' border-none border-white w-64 outline-0' type="text" />
-        <SearchIcon className=' rounded-full p-1 text-white bg-[#FF385C]'/>
       </div>
-      <div className='flex items-center justify-between w-[20vw] mr-20'>
-        <p className=' text-base font-bold'>Become a host</p>
-        <LanguageIcon className=''/>
-        <div className='flex items-center w-24 p-1 h-14  rounded-full boreder border-[#DDDDDD]  border-2 shadow-lg '>
-        <MenuRoundedIcon className='m-1'/>
-        <Avatar className='m-1'/>
+
+      {/* middle */}
+      <div className=' flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
+        <input className=' flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400' type="text" placeholder='Start your Search' />
+        <SearchIcon className="hidden md:inline-flex h-4  bg-red-400  text-white rounded-full p-1 cursor-pointer md:mx-2" />
+      </div>
+
+      {/* right */}
+      <div className='flex items-center justify-end space-x-4 text-gray-500'>
+        <p className=' hidden md:inline cursor-pointer'>Become a host</p>
+        <LanguageIcon className='h-6 cursor-pointer'/>
+        <div className='flex items-center space-x-2 border-2 p-2  rounded-full'>
+        <MenuRoundedIcon className='h-6'/>
+        <Avatar className='h-6'/>
         </div>
       </div>
-    </div>
-    <hr className='w-[100%] border-[#DDDDDD] shadow-md '/>
-    </>
+    
+    {/* <hr className='w-[100%] border-[#DDDDDD] shadow-md '/> */}
+    </header>
   )
 }
 
